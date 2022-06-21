30cd330be73e4402ec3a913014c9bfdc07c701b6
aguilanbon <aguilanbon@gmail.com>
2022-06-21 16:02:09 +0800

    moved styled for login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import InputComponent from '../components/InputComponent'
+import { Input, InputDiv, InputLabel, RightCol, SignInText, StyledButton } from '../components/styles/RightCol.styled'
 import { LoginContainer } from '../components/styles/LoginContainer.styled'
 
 function Login() {
     return (
         <LoginContainer>
             <Hero />
-            <InputComponent />
+            <RightCol>
+                <SignInText>Log In to your Account</SignInText>
+                <InputDiv>
+                    <InputLabel>Email</InputLabel>
+                    <Input type='text' placeholder='Your Email here...'></Input>
+                    <InputLabel>Password</InputLabel>
+                    <Input type='password' placeholder='Your password here...'></Input>
+                    <StyledButton>Log in</StyledButton>
+                </InputDiv>
+            </RightCol>
         </LoginContainer>
     )
 }
